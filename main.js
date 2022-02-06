079778f47411b503d565019baa61d975e568b539
wallyson ruan <wallysonruan@gmail.com>
2022-02-06 01:32:58 -0300

    Im not satisfied with the functions 'search IP', Ill change it for a RegEx validation. Also, Ive to put the map under the result boxes

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
-const personalToken = "pk.eyJ1Ijoid2FsbHlzb25ydWFuIiwiYSI6ImNremFsdjduejI1aTcycHIxd2d4MG9zeG8ifQ.ARlUGYo0jle4wk8WPtgbQw";
+const input = document.getElementById("search-bar__input"),
+bttn = document.getElementById("search-bar__bttn");
 
+
+//I plan to substitute the entire function below for a RegEx validation
+input.addEventListener("input", numberOrDotOnly => {
+    let ipAdress = input.value,
+    ipAdressLastChar = input.value.charAt(input.value.length - 1);
+
+    if(ipAdress.length == 1){
+        if(isNaN(parseInt(ipAdress))){
+            input.value = "";
+            }
+    }
+    else{
+        if(isNaN(parseInt(ipAdressLastChar)) && ipAdressLastChar != "."){
+            input.value = ipAdress.slice(0, ipAdress.length - 1)
+        }
+    }
+})
+
+bttn.addEventListener("click", searchIp => {
+    let ipAdress = input.value,
+    firstThree, secondThree, thirdThree, fourthThree;
+
+
+    if(ipAdress.length < 12){
+        document.getElementById("ip").innerHTML = "INCORRECT IP!"
+    }else{
+        if(ipAdress.length == 15){
+            firstThree = ipAdress.slice(0,3),
+            secondThree = ipAdress.slice(4,7),
+            thirdThree = ipAdress.slice(8,11),
+            fourthThree = ipAdress.slice(12,15);
+        }else{
+            firstThree = ipAdress.slice(0,3),
+            secondThree = ipAdress.slice(3,6),
+            thirdThree = ipAdress.slice(6,9),
+            fourthThree = ipAdress.slice(9,12);
+        }
+    }
+
+        console.log(firstThree + "-" + secondThree + "-" + thirdThree + "-" + fourthThree)
+})
+
+
+const personalToken = "pk.eyJ1Ijoid2FsbHlzb25ydWFuIiwiYSI6ImNremFsdjduejI1aTcycHIxd2d4MG9zeG8ifQ.ARlUGYo0jle4wk8WPtgbQw";
 //Set the location position to be shown on the map
 let map = L.map('map').setView([37.38605, -122.08385], 13);
 
