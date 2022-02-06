60e2a1707fe92bd8134981041aee676dda61eeed
wallyson ruan <wallysonruan@gmail.com>
2022-02-06 20:25:34 -0300

    Ive coded some functions to deal with the API, gotta work on the validation one

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const input = document.getElementById("search-bar__input"),
 bttn = document.getElementById("search-bar__bttn");
 
 
-//I plan to substitute the entire function below for a RegEx validation
-input.addEventListener("input", numberOrDotOnly => {
+//I plan to substitute the entire functions below for a RegEx validation
+const numberOrDotOnly = () => {
     let ipAdress = input.value,
     ipAdressLastChar = input.value.charAt(input.value.length - 1);
 
@@ -17,32 +17,51 @@ input.addEventListener("input", numberOrDotOnly => {
             input.value = ipAdress.slice(0, ipAdress.length - 1)
         }
     }
-})
-
-bttn.addEventListener("click", searchIp => {
+}
+const validateIpAdress = () => {
     let ipAdress = input.value,
     firstThree, secondThree, thirdThree, fourthThree;
 
-
-    if(ipAdress.length < 12){
-        document.getElementById("ip").innerHTML = "INCORRECT IP!"
+    if(ipAdress.length == 15){
+        firstThree = ipAdress.slice(0,3),
+        secondThree = ipAdress.slice(4,7),
+        thirdThree = ipAdress.slice(8,11),
+        fourthThree = ipAdress.slice(12,15);
     }else{
-        if(ipAdress.length == 15){
-            firstThree = ipAdress.slice(0,3),
-            secondThree = ipAdress.slice(4,7),
-            thirdThree = ipAdress.slice(8,11),
-            fourthThree = ipAdress.slice(12,15);
-        }else{
-            firstThree = ipAdress.slice(0,3),
-            secondThree = ipAdress.slice(3,6),
-            thirdThree = ipAdress.slice(6,9),
-            fourthThree = ipAdress.slice(9,12);
-        }
+        firstThree = ipAdress.slice(0,3),
+        secondThree = ipAdress.slice(3,6),
+        thirdThree = ipAdress.slice(6,9),
+        fourthThree = ipAdress.slice(9,12);
     }
 
-        console.log(firstThree + "-" + secondThree + "-" + thirdThree + "-" + fourthThree)
-})
+    ipAdress = firstThree + "." + secondThree + "." + thirdThree + "." + fourthThree;
 
+    return ipAdress
+}
+const searchIpAdress = (ip) =>{
+    fetch(`https://geo.ipify.org/api/v1?apiKey=at_Vd9V7RfXcNnnNbl69el94S51ORuj1&ipAddress=${ip}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        showIpAdress(data);
+    })
+}
+const showIpAdress = (data) =>{
+    document.getElementById("ip").innerHTML = data.ip;
+
+    document.getElementById("location").innerHTML = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+
+    document.getElementById("timezone").innerHTML = data.location.timezone;
+
+    document.getElementById("isp").innerHTML = data.isp;
+}
+
+input.addEventListener("input", go => {
+    numberOrDotOnly();
+})
+bttn.addEventListener("click", go => {
+    searchIpAdress(validateIpAdress());
+});
 
 const personalToken = "pk.eyJ1Ijoid2FsbHlzb25ydWFuIiwiYSI6ImNremFsdjduejI1aTcycHIxd2d4MG9zeG8ifQ.ARlUGYo0jle4wk8WPtgbQw";
 //Set the location position to be shown on the map
